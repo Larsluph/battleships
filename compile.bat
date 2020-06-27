27343a57816686440bf7379aea479489f5e86fd6
Larsluph <remarso59@sfr.fr>
2020-06-27 23:31:31 +0200

    preparing translation update

--- compile.bat
+++ compile.bat
@@ -1,5 +1,6 @@
-pyinstaller -F --distpath ./compiled main.py
+pyinstaller -F --distpath .\compiled main.py
 
 rmdir /S /Q build
 rmdir /S /Q __pycache__
 del main.spec
+xcopy /E /C /Y /I .\locales\ .\compiled\locales\
