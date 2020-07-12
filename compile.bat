b427c590bcda717e5d26040ab01b887565d491af
Larsluph <remarso59@sfr.fr>
2020-07-12 12:31:56 +0200

    fixed compile script

--- compile.bat
+++ compile.bat
@@ -3,4 +3,4 @@ pyinstaller -F -i icon.ico --distpath .\compiled main.py
 rmdir /S /Q build
 rmdir /S /Q __pycache__
 del main.spec
-xcopy /E /C /Y /I .\locales\ .\compiled\locales\
+xcopy /E /C /Y /I .\locales .\compiled\locales
